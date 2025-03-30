7074dbb945148d1aa2ca9e4289ed8c2a9534f1ee
kundan kumar <kundankumar64355@gmail.com>
2025-03-31 00:38:37 +0530

    update the method present in TaskController according to user for eg. previously getAll method  provide all the avialable tasks but now it will return only all tasks of a particular user

--- src/main/java/com/example/Taskmanager/controller/TaskController.java
+++ src/main/java/com/example/Taskmanager/controller/TaskController.java
@@ -1,15 +1,15 @@
 package com.example.Taskmanager.controller;
 
 import com.example.Taskmanager.entity.TaskEntry;
+import com.example.Taskmanager.entity.User;
 import com.example.Taskmanager.service.TaskEntryService;
+import com.example.Taskmanager.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,23 +22,25 @@ public class TaskController {
     @Autowired
     private TaskEntryService taskEntryService;
 
-    @GetMapping
-    public ResponseEntity<List<TaskEntry>> getAll() {
-        List<TaskEntry> all = taskEntryService.getAll();
-        if (all != null && !all.isEmpty()){
-            return new ResponseEntity<>(all , HttpStatus.OK);
+    @Autowired
+    private UserService userService;
 
+    @GetMapping("/{username}")
+    public ResponseEntity<List<TaskEntry>> getAllTasksOfUser(@PathVariable String username) {
+        User user = userService.findByUsername(username);
+        List<TaskEntry> all = user.getTaskEntries();
+        if (all != null && !all.isEmpty()) {
+            return new ResponseEntity<>(all, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
-    @PostMapping
-    public ResponseEntity<?> createTask(@RequestBody TaskEntry myEntry) {
+    @PostMapping("/{username}")
+    public ResponseEntity<?> createTask(@RequestBody TaskEntry myEntry, @PathVariable String username) {
         log.info("Received request to create a new task: {}", myEntry);
         try {
-            myEntry.setDate(LocalDateTime.now());
-            taskEntryService.saveEntry(myEntry);
+            taskEntryService.saveEntry(myEntry, username);
             return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
         } catch (Exception e) {
             log.error("error in creating new task {}", myEntry.getContent(), e);
@@ -60,26 +62,28 @@ public class TaskController {
         }
     }
 
-    @DeleteMapping("id/{myId}")
-    public ResponseEntity<?> deleteTaskById(@PathVariable ObjectId myId) {
+    @DeleteMapping("id/{username}/{myId}")
+    public ResponseEntity<?> deleteTaskById(@PathVariable ObjectId myId, @PathVariable String username) {
         try {
-            taskEntryService.deleteById(myId);
-            return new ResponseEntity<>(myId , HttpStatus.OK);
-        }catch (Exception e){
+            User user = userService.findByUsername(username);
+            taskEntryService.deleteById(myId, username);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
     }
 
-    @PutMapping("id/{myId}")
-    public ResponseEntity<TaskEntry> updateById(@PathVariable ObjectId myId, @RequestBody TaskEntry newEntry) {
-            TaskEntry old = taskEntryService.findById(myId).orElse(null);
-            if (old != null) {
-                old.setTitle(!newEntry.getTitle().isEmpty() ? newEntry.getTitle() : old.getTitle());
-                old.setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : old.getContent());
-                taskEntryService.saveEntry(old);
-                return  new ResponseEntity<>(old , HttpStatus.OK);
-            }
-           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @PutMapping("id/{username}/{myId}")
+    public ResponseEntity<TaskEntry> updateById(@PathVariable ObjectId myId, @RequestBody TaskEntry newEntry, @PathVariable String username) {
+
+        TaskEntry old = taskEntryService.findById(myId).orElse(null);
+        if (old != null) {
+            old.setTitle(!newEntry.getTitle().isEmpty() ? newEntry.getTitle() : old.getTitle());
+            old.setContent(newEntry.getContent() != null && !newEntry.getContent().isEmpty() ? newEntry.getContent() : old.getContent());
+            taskEntryService.saveEntry(old);
+            return new ResponseEntity<>(old, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     // above we didn't check the condition newEntry.getTitle() != null because title is already
--- src/main/java/com/example/Taskmanager/service/TaskEntryService.java
+++ src/main/java/com/example/Taskmanager/service/TaskEntryService.java
@@ -6,12 +6,14 @@ package com.example.Taskmanager.service;
 // stateless means : No internal memory of previous interactions
 
 import com.example.Taskmanager.entity.TaskEntry;
+import com.example.Taskmanager.entity.User;
 import com.example.Taskmanager.repository.TaskRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,12 +24,23 @@ public class TaskEntryService {
     @Autowired
     private TaskRepository taskRepository;
 
-    public void saveEntry(TaskEntry taskEntry) {
+    @Autowired
+    private UserService userService;
+
+    public void saveEntry(TaskEntry taskEntry, String username) {
         try {
-            taskRepository.save(taskEntry);
-        }catch (Exception e){
-            log.error("A task with the same title and content already exists!" , e);
+            User user = userService.findByUsername(username);
+            taskEntry.setDate(LocalDateTime.now());
+            TaskEntry saved = taskRepository.save(taskEntry);
+            user.getTaskEntries().add(saved);
+            userService.saveUser(user);
+        } catch (Exception e) {
+            log.error("A task with the same title and content already exists!", e);
         }
+    }
+
+    public void saveEntry(TaskEntry taskEntry) {
+        taskRepository.save(taskEntry);
     }
 
     public List<TaskEntry> getAll() {
@@ -38,8 +51,11 @@ public class TaskEntryService {
         return taskRepository.findById(myId);
     }
 
-    public  void deleteById(ObjectId myId){
-         taskRepository.deleteById(myId);
+    public void deleteById(ObjectId myId, String username) {
+        User user = userService.findByUsername(username);
+        user.getTaskEntries().removeIf(x -> x.getId().equals(myId));
+        userService.saveUser(user);
+        taskRepository.deleteById(myId);
     }
 
 }
